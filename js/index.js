b6b6af3dd45c08078a97ddb3b410756fba715d93
Marco Whyte <mwhyte@umass.edu>
2020-01-13 19:32:25 -0600

    finished with append and br lines

--- js/index.js
+++ js/index.js
@@ -9,7 +9,7 @@ const siteContent = {
     "img-src": "img/logo.png"
   },
   "cta": {
-    "h1": "DOM Is Awesome",
+    "h1": "DOM <br>Is <br>Awesome",
     "button": "Get Started",
     "img-src": "img/header-img.png"
   },
@@ -28,7 +28,7 @@ const siteContent = {
   },
   "contact": {
     "contact-h4" : "Contact",
-    "address" : "123 Way 456 Street Somewhere, USA",
+    "address" : "123 Way 456 Street <br>Somewhere, USA",
     "phone" : "1 (888) 888-8888",
     "email" : "sales@greatidea.io",
   },
@@ -58,6 +58,18 @@ const fillingNavBar = function(content){
   });
 }
 fillingNavBar(siteContent.nav);
+
+//navBar New additions
+const navigation = document.querySelector('nav');
+navBar.forEach(link => link.style.color = "green");
+const first = document.createElement('a');
+const last = document.createElement('a');
+first.textContent = "Prepend";
+last.textContent = "Append";
+first.style.color = "green";
+last.style.color = "green";
+navigation.appendChild(last);
+navigation.prepend(first);
 
 
 
